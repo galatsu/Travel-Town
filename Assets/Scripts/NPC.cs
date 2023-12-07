c36ef07c12761998717141f240c8279fad664c36
Steven Quacles <Sttrevens@126.com>
2023-12-07 11:41:59 -0500

    Some minor changes

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -13,13 +13,26 @@ public class NPC : MonoBehaviour
     public float fadeDuration = 1.0f;
     public int dialogueIndex = 0;
 
+    public float movementThreshold = 0f;
+    private Vector3 lastPosition;
+
+    private void Start()
+    {
+        lastPosition = player.position;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (!dialogueTriggered && Vector3.Distance(player.position, transform.position) <= activationDistance)
+        if (Vector3.Distance(transform.position, lastPosition) <= movementThreshold)
         {
-            StartCoroutine(StartDialogueWithFade());
+            if (!dialogueTriggered && Vector3.Distance(player.position, transform.position) <= activationDistance)
+            {
+                StartCoroutine(StartDialogueWithFade());
+            }
         }
+
+        lastPosition = transform.position;
     }
 
     IEnumerator StartDialogueWithFade()
